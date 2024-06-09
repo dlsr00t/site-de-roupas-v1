97361d2743930584c61184cabcf5c3ed2743f23c
Douglas Luiz <douglas@Douglass-MacBook-Air.local>
2024-06-09 20:20:04 -0300

    card em funcionamento(beta)

--- src/pages/Components/cards.jsx
+++ src/pages/Components/cards.jsx
@@ -8,35 +8,21 @@ function Cards(name, price, image, description){
 }
 */
 
-function Cards(props){
-    const estilo = {
-        display:"flex",
-        position:"relative",
-        flexDirection:"column",
-        overflow:"hidden",
-        boxSizing: "border-box",
-        
-    }
-    const estiloInfo = {
-        color:"red",
-        display:"flex",
-        position:"absolute",
-        flexDirection:"column",
-        alignItems:"flex-start",
-        transform: "translate(70%, 70%)",
-        boxSizing: "border-box"
-    }
-    return(
-    <div id = "parent" style={estilo}>
-        <img src={props.image} style={{maxWidth: "250px"}}>
+import './cards.css';
 
-        </img>
-        <div style={estiloInfo}>
-            <h3>{props.name}</h3>
-            <h1>{props.price}</h1>
-            <p>{props.description}</p>
+function Cards(props){
+    
+    return(
+        <div className="parent">
+            <img src={props.image}>
+
+            </img>
+            <div className="child">
+                <h3>{props.name}</h3>
+                <h1>{props.price}</h1>
+                <p>{props.description}</p>
+            </div>
         </div>
-    </div>
     )
 }
 
